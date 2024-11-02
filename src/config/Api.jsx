b3ecd408041aa48c820b510f0e5c791357849db4
drianodev <adrianomendes661@gmail.com>
2024-11-02 06:57:24 -0300

    add vercel json

--- src/config/Api.jsx
+++ src/config/Api.jsx
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 const httpClient = axios.create({
     // baseURL: 'http://localhost:8080'
-    baseURL: import.meta.env.VITE_BASE_URL
+    // baseURL: import.meta.env.VITE_BASE_URL
+    baseURL: '/api'
 })
 
 export const get = (url) => {
